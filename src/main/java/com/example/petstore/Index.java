01e36e66ce18a4264a08105466580cc6c26680f5
nurzhasj <montishok@gmail.com>
2022-04-29 01:22:22 +0600

    Logged in user was identified and Index page was fixed

--- src/main/java/com/example/petstore/Index.java
+++ src/main/java/com/example/petstore/Index.java
@@ -3,6 +3,8 @@ package com.example.petstore;
 import com.example.petstore.entities.User;
 import com.example.petstore.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,7 +41,9 @@ public class Index {
 
     @GetMapping("/my-account.html")
     public String my_account(Model model) {
-        return "my-account.html";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("curLogIn", userService.getCurrentlyLoggedInUser(authentication));
+        return "my-account";
     }
 
 //    @GetMapping("/cart.html")
